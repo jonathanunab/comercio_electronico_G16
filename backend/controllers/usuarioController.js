89d1e12a276dd84119658d186c2a0e54630d6b9b
jonathanunab <119382990+jonathanunab@users.noreply.github.com>
2022-12-04 17:01:03 +0000

    Se modifica el controlador para agregar usuarios a la BD

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,17 @@
 const Usuario = require('../models/usuario');
 
 exports.crearUsuario = async (req, res) => {
-    console.log(req.body);
-    res.json({ msg: "desde controller post"});
+    //console.log(req.body);
+
+    try {
+        // Crear nuevo usuario
+        let usuario = new Usuario(req.body);
+
+        // Guardar usuario en la BD
+        const usuarioAlmacenado = await usuario.save();
+        res.json(usuarioAlmacenado);
+        
+    } catch (error) {
+        console.log(error);
+    }
 };

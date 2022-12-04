2c194d0b77206ab1253f892753959fae5f39d8c8
jonathanunab <119382990+jonathanunab@users.noreply.github.com>
2022-12-04 22:22:42 +0000

    Se realiza la configuración de la encriptación de contraseña

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,11 +1,24 @@
 const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
     //console.log(req.body);
+    const { password, email } = req.body;
 
     try {
+
+        // Validación de existencia de usuario en la base de datos
+        let usuario = await Usuario.findOne({email});
+
+        if (usuario) {
+            return res.status(400).json({msg:"el usuario ya existe"})
+        }
+
         // Crear nuevo usuario
-        let usuario = new Usuario(req.body);
+        usuario = new Usuario(req.body);
+
+        // encriptacion
+        usuario.password = await bcryptjs.hash(password, 10);
 
         // Guardar usuario en la BD
         const usuarioAlmacenado = await usuario.save();

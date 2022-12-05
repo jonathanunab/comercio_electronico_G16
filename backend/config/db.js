3e3fc5a9044d91044b6652f706d2b1c39bc8e2c3
dBastidas1 <112983668+dBastidas1@users.noreply.github.com>
2022-12-04 21:34:08 -0500

    Actualizacion

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const conectarDB = async () => {
-    try {
+    try{
         const connection = await mongoose.connect(
-            "mongodb://localhost:27017",{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        const url = `${connection.connection.host}:${connection.connection.port}`;
-        console.log(`MongoDB conectado en ${url}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
+            "mongodb+srv://dbastidas:Alianza2022**@cluster0.kxaqgrh.mongodb.net/?retryWrites=true&w=majority",{
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            const url = `${connection.connection.host}:${connection.connection.port}`;
+            console.log(`MongoDB conectado en : ${url}`);
+    }catch (error){
+        console.log(`error: ${error.message}`);
         process.exit(1);
     }
 }
-
 module.exports = conectarDB;
